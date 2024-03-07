f244cd5be2706f8e6138ba1f43212a30f1fb8f93
Geonwook Ham <tomy8964@naver.com>
2024-03-07 13:53:05 +0900

    [level 1] Title: 폰켓몬, Time: 3.33 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){
+            set.add(num);
+        }
+        
+        answer = Math.min(set.size(), nums.length/2);
+        
+        return answer;
+    }
+}
