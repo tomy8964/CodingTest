024c26002d1920f0bcfa683d2ba7ea93e936b7ea
Geonwook Ham <tomy8964@naver.com>
2024-03-13 23:49:43 +0900

    [level 3] Title: 단속카메라, Time: 15.45 ms, Memory: 53.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+
+    public int solution(int[][] routes) {
+        int answer = 0;
+
+        Arrays.sort(routes, Comparator.comparingInt(o -> o[1]));
+        boolean[] visited = new boolean[routes.length];
+
+        int index = 0;
+        for (int i = index; i < routes.length; i++) {
+            if (visited[i]) continue;
+            int start = routes[i][0];
+            int end = routes[i][1];
+            visited[i] = true;
+            for (int j = i + 1; j < routes.length; j++) {
+                if (routes[j][0] <= end) {
+                    visited[j] = true;
+                }
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
