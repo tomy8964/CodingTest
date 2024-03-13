b6fb81ff08c89b68d6d388828b2917a6fe4ad93f
Geonwook Ham <tomy8964@naver.com>
2024-03-13 17:02:29 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.05 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -10,20 +10,29 @@ class Solution {
         int finishedJobs = 0;
         boolean[] finished = new boolean[jobs.length];
         while (finishedJobs < jobs.length) {
+            // 먼저 요청이 들어온 job 부터 돌면서
             for (int i = 0; i < jobs.length; i++) {
+                // 현재 요청을 처리할 수 있는 job이면 queue에 add
                 if (jobs[i][0] <= currentTime && !finished[i]) {
                     queue.add(new Job(jobs[i][0], jobs[i][1]));
                     finished[i] = true;
                 }
             }
+            // 현재 처리할 수 있는 목록들이 들어있는 큐 확인
             if (!queue.isEmpty()) {
+                // 가장 처리시간이 적은 job 부터 poll 되므로
                 Job poll = queue.poll();
+                // 처리 후 현재 시간 업데이트
                 currentTime += poll.processTime;
+                // 이 job의 요청부터 종료까지의 시간 저장
                 time.add(currentTime - poll.startTime);
+                // 완료한 작업 수 증가
                 finishedJobs++;
-            } else currentTime++;
+            } // 현재 처리할 수 있는 job이 없으면 1초 증가
+            else currentTime++;
         }
 
+        // 평균 구함
         int sum = 0;
         for (Integer num : time) {
             sum += num;
@@ -35,7 +44,8 @@ class Solution {
     public class Job {
         int startTime;
         int processTime;
-
+        
+        
         public Job(int startTime, int processTime) {
             this.startTime = startTime;
             this.processTime = processTime;
