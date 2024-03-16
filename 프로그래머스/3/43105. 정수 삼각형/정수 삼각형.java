927184304e74ed96df0c9dc877349ff1e9978045
Geonwook Ham <tomy8964@naver.com>
2024-03-16 18:20:49 +0900

    [level 3] Title: 정수 삼각형, Time: 8.05 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public int solution(int[][] triangle) {
+        int answer = 0;
+
+        // 7
+        // 3 8
+        // 8 1 0
+        // 2 7 4 4
+
+        // dp[0][0] == triangle[0][0]
+        //
+        // dp[1][0] == dp[0][0] + triangle[1][0]
+        // dp[1][1] == dp[0][0] + triangle[1][1]
+        //
+        // dp[2][0] == dp[1][0] + triangle[2][0]
+        // dp[2][1] == Math.max(dp[1][0], dp[1][1]) + triangle[2][1]
+        // dp[2][2] == dp[1][1] + triangle[2][2]
+
+        int N = triangle.length;
+        int[][] dp = new int[N][N];
+
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < N; i++) {
+            int length = triangle[i].length;
+            for (int j = 0; j < length; j++) {
+                if (j - 1 < 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                }
+            }
+        }
+
+        for (int cost : dp[N - 1]) {
+            answer = Math.max(answer, cost);
+        }
+
+        return answer;
+    }
+}
