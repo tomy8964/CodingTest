10af6ee1db2dde39244b6e5bbcbc3abddfe5635a
Geonwook Ham <tomy8964@naver.com>
2024-03-16 18:39:06 +0900

    [level 3] Title: 등굣길, Time: 0.50 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+
+        int[][] dp = new int[n + 1][m + 1];
+        int[][] map = new int[n + 1][m + 1];
+
+        for (int[] p : puddles) {
+            map[p[1]][p[0]] = -1;
+        }
+        dp[1][1] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 && j == 1) continue;
+                if (map[i][j] == -1) continue;
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1_000_000_007;
+            }
+        }
+
+        return dp[n][m] % 1_000_000_007;
+    }
+}
