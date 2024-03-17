f2b95f4a80afd7e364b9d842ebc68d8890a3047a
Geonwook Ham <tomy8964@naver.com>
2024-03-17 18:14:03 +0900

    [level 3] Title: 순위, Time: 14.77 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -3,12 +3,12 @@ class Solution {
 
     public int solution(int n, int[][] results) {
         int answer = 0;
-
         int[][] graph = new int[n + 1][n + 1];
 
         for (int i = 0; i < results.length; i++) {
             int win = results[i][0];
             int lose = results[i][1];
+
             graph[win][lose] = 1;
             graph[lose][win] = -1;
         }
@@ -16,17 +16,13 @@ class Solution {
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
-                    // i가 k를 이기고 K는 j를 이기면
-                    // i는 j를 이긴다
+                    // i가 k에게 이겼으면 k가 이긴 j에게도 이긴다.
                     if (graph[i][k] == 1 && graph[k][j] == 1) {
                         graph[i][j] = 1;
-                        graph[j][i] = -1;
                     }
-                    // i가 k한테 지고 k는 j에게 지면
-                    // i는 j에게 진다
+                    // i가 k에게 졌으면 k가 진 j에게도 진다.
                     if (graph[i][k] == -1 && graph[k][j] == -1) {
                         graph[i][j] = -1;
-                        graph[j][i] = 1;
                     }
                 }
             }
@@ -35,9 +31,12 @@ class Solution {
         for (int[] player : graph) {
             int count = 0;
             for (int game : player) {
-                if (game == -1 || game == 1) count++;
+                if (game != 0) {
+                    count++;
+                }
             }
-            if (count == n-1) answer++;
+            // 자기를 제외한 나머지 경기 결과에 대해 안다. -> 순위를 정할 수 있다.
+            if (count == n - 1) answer++;
         }
 
         return answer;
