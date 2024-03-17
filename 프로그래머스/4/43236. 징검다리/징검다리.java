d1fc2a78b8cb9cfe9d9f669c8fe9c7910a5a858e
Geonwook Ham <tomy8964@naver.com>
2024-03-17 19:13:35 +0900

    [level 4] Title: 징검다리, Time: 30.81 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -5,35 +5,31 @@ class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
         Arrays.sort(rocks);
-        // 각 바위 사이의 거리의 최소값을 mid로 설정한 뒤 제거되는 바위 갯수로 판단하여 최대의 최소값을 구한다.
-        int start = 0;
-        int end = distance;
+
+        long end = distance;
+        long start = 0;
 
         while (start <= end) {
-            // 설정할 바위 간의 거리의 최소값
-            int mid = (start + end) / 2;
-            // 제거해야할 돌의 갯수
-            int sum = 0;
-            int prev = 0;
-
-            for (int i = 0; i < rocks.length; i++) {
-                if (rocks[i] - prev < mid) {
-                    sum++;
-                } else prev = rocks[i];
-                if (sum > n) break;
+            long mid = (start + end) / 2;
+            int removeRocks = 0;
+            int prevRock = 0;
+            for (int rock : rocks) {
+                if (rock - prevRock < mid) {
+                    removeRocks++;
+                } else prevRock = rock;
             }
 
-            // 마지막 돌도 제거해야 하는지 확인
-            if (distance - prev < mid) {
-                sum++;
+            // 제거하지 않은 돌들 중 마지막 돌과 distance 사이의 거리도 확인
+            if (distance - prevRock < mid) {
+                removeRocks++;
             }
 
-            // 제거한 돌의 갯수가 더 많으면 거리를 줄여야 한다.
-            if (sum > n) {
+            // 제거한 돌이 많으면 최소 거리를 줄인다.
+            if (removeRocks > n) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
-                answer = mid;
+                answer = (int) mid;
             }
         }
 
