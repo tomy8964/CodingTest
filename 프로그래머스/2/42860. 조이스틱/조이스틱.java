8785b03f9565b723f1b3eaa44acd7142bbf78b81
Geonwook Ham <tomy8964@naver.com>
2024-03-17 18:38:42 +0900

    [level 2] Title: 조이스틱, Time: 0.09 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,26 +2,23 @@ class Solution {
 
     public int solution(String name) {
         int answer = 0;
-        // 1. 계속 오른쪽으로 가면서 바꾸기
-        int move = name.length() - 1;
+        // 오른쪽으로만 가면서 바꿀 경우
+        int right = name.length() - 1;
 
         for (int i = 0; i < name.length(); i++) {
-            // 현재 위치의 A를 주어진 알파벳으로 바꾸기 위한 최소 횟수
-            int up = name.charAt(i) - 'A';
-            int down = 26 - (name.charAt(i) - 'A');
-            answer += Math.min(up, down);
+            answer += Math.min((26 - (name.charAt(i) - 'A')), (name.charAt(i) - 'A'));
+
 
             int next = i + 1;
             while (next < name.length() && name.charAt(next) == 'A') {
                 next++;
             }
-
-            // 2. 오른쪽으로 가다가 연속된 A를 만나면 왼쪽으로 돌아가면서 바꾸기
-            move = Math.min(move, i * 2 + name.length() - next);
-            // 3. 왼쪽으로 가다가 연속된 A를 만나면 오른쪽으로 돌아가면서 바꾸기
-            move = Math.min(move, (name.length() - next) * 2 + i);
+            right = Math.min(right, i * 2 + (name.length() - next));
+            right = Math.min(right, (name.length() - next) * 2 + i);
         }
 
-        return answer + move;
+        answer += right;
+
+        return answer;
     }
 }
