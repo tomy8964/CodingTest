8c1c87893efada3a45148eea7dc533ca075658ec
Geonwook Ham <tomy8964@naver.com>
2024-03-17 18:18:54 +0900

    [level 3] Title: 입국심사, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,25 +2,23 @@ import java.util.*;
 class Solution {
 
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
-
-        // 제일 오래 걸리는 사람에게 n명 전부 심사를 받는 최악의 상황에 걸릴 시간
+        // 가장 오래 걸리는 사람에게 n명 전부 심사 받을 경우
         long end = (long) times[times.length - 1] * n + 1;
         long start = 0;
 
         while (start < end) {
             long mid = (start + end) / 2;
             long sum = 0;
-            for (int i = 0; i < times.length; i++) {
-                sum += mid / times[i];
+
+            // mid 시간이 걸릴때 처리할 수 있는 사람 수 sum
+            for (int time : times) {
+                sum += mid / time;
             }
 
-            // mid 시간 동안 처리한 사람의 명 수(sum)이 부족할때
+            // 시간을 더 늘려서 더 많은 사람을 처리해야 한다.
             if (sum < n) {
                 start = mid + 1;
-            } else {
-                end = mid;
-            }
+            } else end = mid;
         }
 
         return end;
