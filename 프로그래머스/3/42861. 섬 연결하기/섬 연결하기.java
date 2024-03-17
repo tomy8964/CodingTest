72312d88bce221e869597850c43f4b377ca4d441
Geonwook Ham <tomy8964@naver.com>
2024-03-17 18:08:59 +0900

    [level 3] Title: 섬 연결하기, Time: 1.96 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,40 +4,32 @@ class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
 
-        int[] parent = new int[n];
-        for (int i = 0; i < parent.length; i++) {
+        Arrays.sort(costs, Comparator.comparingInt(o -> o[2]));
+
+        int[] parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
             parent[i] = i;
         }
-
-        Arrays.sort(costs, (Comparator.comparingInt(o -> o[2])));
-
         for (int i = 0; i < costs.length; i++) {
-            int node1 = costs[i][0];
-            int node2 = costs[i][1];
-            int parent1 = findParent(node1, parent);
-            int parent2 = findParent(node2, parent);
-
+            int parent1 = findParent(costs[i][0], parent);
+            int parent2 = findParent(costs[i][1], parent);
             if (parent1 != parent2) {
                 answer += costs[i][2];
-                union(parent, node1, node2);
+                union(parent1, parent2, parent);
             }
         }
-
         return answer;
     }
 
-    private void union(int[] parent, int node1, int node2) {
-        int parent1 = findParent(node1, parent);
-        int parent2 = findParent(node2, parent);
-        if (parent1 < parent2) {
-            parent[parent2] = parent1;
-        } else parent[parent1] = parent2;
+    private void union(int node1, int node2, int[] parent) {
+        if (parent[node1] < parent[node2]) {
+            parent[node2] = parent[node1];
+        } else parent[node1] = parent[node2];
     }
 
     private int findParent(int node, int[] parent) {
         if (parent[node] == node) {
-            return node;
-        }
-        return findParent(parent[node], parent);
+            return parent[node];
+        } else return findParent(parent[node], parent);
     }
 }
