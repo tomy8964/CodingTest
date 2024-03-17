4da85195622def4872308cfc80f2fff37c3440ad
Geonwook Ham <tomy8964@naver.com>
2024-03-17 23:29:19 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,23 @@
 class Solution {
-
-    static boolean[] visited;
     static int answer;
 
     public int solution(int k, int[][] dungeons) {
         answer = -1;
-        visited = new boolean[dungeons.length];
-        dfs(dungeons, k, 0);
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(visited, dungeons, k, 0);
         return answer;
     }
 
-    public void dfs(int[][] dungeons, int k, int count) {
+    public void dfs(boolean[] visited, int[][] dungeons, int k, int count) {
+        answer = Math.max(answer, count);
         for (int i = 0; i < dungeons.length; i++) {
-            // 현재 남은 피로도가 던전의 최소 필요도보다 크거나 같은지
             if (!visited[i]) {
-                if (k >= dungeons[i][0]) {
+                if (dungeons[i][0] <= k) {
                     visited[i] = true;
-                    dfs(dungeons, k - dungeons[i][1], count + 1);
+                    dfs(visited, dungeons, k - dungeons[i][1], count + 1);
                     visited[i] = false;
                 }
             }
         }
-        answer = Math.max(answer, count);
     }
 }
