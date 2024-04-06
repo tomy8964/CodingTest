eeb153d81b327c5f2c4acd93a44f0ce85ea5608b
Geonwook Ham <tomy8964@naver.com>
2024-04-06 15:34:18 +0900

    [level 3] Title: 기지국 설치, Time: 2.68 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -2,21 +2,28 @@ class Solution {
 
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
+
+        // 전파가 안닿는 구간 길이 확인
         int left = 0;
-        int right = n;
+        int right;
+        // 구간에 기지국을 최소로 설치하는 갯수
+        // 구간 / (2w+1) 개
         for (int s : stations) {
-            right = s - 1 - w - 1;
-            answer += num(right - left + 1, w);
-            left = s - 1 + w + 1;
-            right = s - 1 + w + 1;
+            right = (s - 1) - w;
+            answer += count(right - left, w);
+            left = s + w;
         }
-        if (n > right) {
-            answer += num(n - right, w);
+
+        // 마지막으로 설치된 기지국 오른쪽에 전파가 닿지 않는 구간이 있을 때
+        if (left < n) {
+            right = n;
+            answer += count(right - left, w);
         }
+
         return answer;
     }
 
-    private int num(int length, int w) {
+    private int count(int length, int w) {
         return (int) Math.ceil((double) length / (2 * w + 1));
     }
 }
