618f6938c18b0fe23a1bd8d96d657f5c34c50197
Geonwook Ham <tomy8964@naver.com>
2024-04-06 16:11:46 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.40 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 0.36 ms
+메모리: 75 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 03일 18:30:48
+2024년 04월 06일 16:11:43
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -5,43 +5,57 @@ class Solution {
     static int[] yMove = {0, 0, -1, 1};
 
     public int solution(int[][] board) {
-        // 0 : 상하, 1 : 좌우
+        // 0 : 상하, 1: 좌우 로 왔을 때의 비용
         int[][][] cost = new int[board.length][board.length][2];
-        for (int[][] c : cost) {
-            for (int[] cc : c) {
-                Arrays.fill(cc, Integer.MAX_VALUE - 1000);
+        for (int[][] co : cost) {
+            for (int[] c : co) {
+                c[0] = Integer.MAX_VALUE - 1000;
+                c[1] = Integer.MAX_VALUE - 1000;
             }
         }
+
+        // 출발할때의 비용은 0으로 초기화
+        cost[0][0][0] = 0;
+        cost[0][0][1] = 0;
+
         bfs(new Point(0, 0), board, cost);
+
         return Math.min(cost[board.length - 1][board.length - 1][0], cost[board.length - 1][board.length - 1][1]);
     }
 
     public void bfs(Point start, int[][] board, int[][][] cost) {
         Queue<Point> queue = new LinkedList<>();
-        cost[start.x][start.y][0] = 0;
-        cost[start.x][start.y][1] = 0;
         queue.add(start);
         while (!queue.isEmpty()) {
             Point current = queue.poll();
-            // 상하 방향에서 온 경우의 가격
+            // 현재 도로에 상하로 온 경우의 비용
             int currentPrice1 = cost[current.x][current.y][0];
-            // 좌우 방향에서 온 경우의 가격
+            // 현재 도로에 좌우로 온 경우의 비용
             int currentPrice2 = cost[current.x][current.y][1];
+
             for (int i = 0; i < 4; i++) {
                 int newX = current.x + xMove[i];
                 int newY = current.y + yMove[i];
 
-                if (newX < 0 || newY < 0 || newX >= board.length || newY >= board.length || board[newX][newY] == 1)
-                    continue;
+                if (newX < 0 || newY < 0 || newX >= board.length || newY >= board.length) continue;
+                if (board[newX][newY] == 1) continue;
 
-                // 상하 방향에서 옴
+                // 비용 비교
+                // 상하 방향에서 온 경우
                 if (i < 2) {
+                    // 현재 위치로 상하로 와서 상하로 새로운 위치로 가는 비용과
+                    // 현재 위치로 좌우로 와서 상하로 새로운 위치로 가는 비용 중
+                    // 적은 비용을 가지고 새로운 위치로 가는 비용을 비교
                     int newPrice = Math.min(currentPrice1 + 100, currentPrice2 + 600);
                     if (cost[newX][newY][0] > newPrice) {
                         cost[newX][newY][0] = newPrice;
                         queue.add(new Point(newX, newY));
                     }
-                } else {
+                } // 좌우 방향에서 온 경우
+                else {
+                    // 현재 위치로 상하로 와서 좌우로 새로운 위치로 가는 비용과
+                    // 현재 위치로 좌우로 와서 좌우로 새로운 위치로 가는 비용 중
+                    // 적은 비용을 가지고 새로운 위치로 가는 비용을 비교
                     int newPrice = Math.min(currentPrice1 + 600, currentPrice2 + 100);
                     if (cost[newX][newY][1] > newPrice) {
                         cost[newX][newY][1] = newPrice;
