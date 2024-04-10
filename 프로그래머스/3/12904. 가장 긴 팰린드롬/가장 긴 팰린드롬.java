357740232e9e7a6129bfae4150bfa84d3ad3cf90
Geonwook Ham <tomy8964@naver.com>
2024-04-10 15:53:40 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.13 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -4,22 +4,22 @@ class Solution
     public int solution(String s) {
         int answer = 1;
 
-        // String s가 팰린드롬인 경우 가장 길다.
+        // s가 팰린드롬인 경우 가장 길다.
         int length = s.length();
 
-        // 길이를 1씩 줄이면서 팰린드롬을 찾는다.
-        for (int i = length; i > 1; i--) {
-            // 윈도우를 앞에서부터 한칸씩 오른쪽으로 움직이면서 팰린드롬인 경우를 찾는다.
-            for (int start = 0; start + i <= length; start++) {
-                // 이번 윈도우가 팰린드롬인지 확인
+        // 윈도우를 하나씩 줄이면서 팰린드롬을 찾는다.
+        for (int window = length; window > 0; window--) {
+            // 왼쪽에서부터 윈도우를 움직이며 팰린드롬을 찾는다.
+            for (int start = 0; start + window <= length; start++) {
+                // 이번 윈도우의 위치가 팰린드롬인지 확인
                 boolean palindrome = true;
-                for (int j = 0; j < i / 2; j++) {
-                    if (s.charAt(start + j) != s.charAt(start + i - j - 1)) {
+                for (int j = 0; j < window / 2; j++) {
+                    if (s.charAt(start + j) != s.charAt(start + window - j - 1)) {
                         palindrome = false;
                         break;
                     }
                 }
-                if (palindrome) return i;
+                if (palindrome) return window;
             }
         }
 
