3e6dc9e216fe5e4ff71918ec43c6747f0469eb3d
Geonwook Ham <tomy8964@naver.com>
2024-04-15 21:42:18 +0900

    [level 3] Title: 풍선 터트리기, Time: 26.03 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,31 +1,39 @@
 class Solution {
-
     public int solution(int[] a) {
         int answer = 0;
-        // 번호가 더 작은 풍선을 터트리는 행위는 최대 1번만 할 수 있습니다.
-        // 내 양쪽의 값이 둘다 나보다 작다면 절대 끝까지 남을 수 없음
-        // 양쪽 끝의 풍선은 마지막까지 무조건 남을 수 있음
+
+        // 인접한 두 풍선 중에서 번호가 더 작은 풍선을 터트리는 행위는 최대 1번만 할 수 있습니다.
+        // 즉, 내 양쪽의 값이 둘 다 나보다 작다면 절대 끝까지 남을 수 없음 (작은건 1번만 터트릴 수 있으므로)
+
+        // 제일 왼쪽부터 나까지의 번호의 최소값이 나일 경우
+        // 혹은, 나부터 제일 오른쪽 까지의 번호의 최소값이 나일 경우
+        // 둘 중 하나의 경우이면 나는 끝까지 남을 수 있다.
+
+        // 양쪽 끝의 풍선은 무조건 끝까지 남을 수 있다.
         answer += 2;
 
-        // i 번째 풍선의 왼쪽에 올 수 있는 최소값을 구함
-        int[] leftMin = new int[a.length];
-        leftMin[0] = a[0];
+        // 제일 왼쪽부터 i까지의 번호의 최소값
+        int[] left = new int[a.length];
+        left[0] = a[0];
+        // 나부터 제일 오른쪽 까지의 번호의 최소값
+        int[] right = new int[a.length];
+        right[right.length - 1] = a[a.length - 1];
         for (int i = 1; i < a.length; i++) {
-            leftMin[i] = Math.min(leftMin[i - 1], a[i]);
+            // 제일 왼쪽부터 i까지의 번호의 최소값
+            left[i] = Math.min(left[i - 1], a[i]);
         }
-        // i 번째 풍선의 오른쪽에 올 수 있는 최소값을 구함
-        int[] rightMin = new int[a.length];
-        rightMin[a.length - 1] = a[a.length - 1];
         for (int i = a.length - 2; i > -1; i--) {
-            rightMin[i] = Math.min(rightMin[i + 1], a[i]);
+            // 나부터 제일 오른쪽 까지의 번호의 최소값
+            right[i] = Math.min(right[i + 1], a[i]);
         }
 
-        // -> 0부터 i까지의 풍선 배열 중 최소값과 i 번째 풍선의 번호를 비교
-        // -> i 부터 끝까지의 풍선 배열 중 최소값과 i 번째 풍선의 번호를 비교
         for (int i = 1; i < a.length - 1; i++) {
-            // 둘 중 하나만 나와 같다면
-            // i 번쨰 풍선은 마지막까지 남을 수 있음
-            if (a[i] == leftMin[i] || a[i] == rightMin[i]) answer++;
+            // 제일 왼쪽부터 나까지의 번호의 최소값이 나일 경우
+            // 혹은, 나부터 제일 오른쪽 까지의 번호의 최소값이 나일 경우
+            // 둘 중 하나의 경우이면 나는 끝까지 남을 수 있다.
+            if (left[i] == a[i] || right[i] == a[i]) {
+                answer++;
+            }
         }
         return answer;
     }
