7294b6bb138d0ec28b1f90e41c3a6b71230c209b
Geonwook Ham <tomy8964@naver.com>
2024-04-17 17:38:53 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.46 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -12,12 +12,12 @@ class Solution {
             double b = (weight * 2.0) / 3.0;    // 2/3인 경우
             double c = (weight * 1.0) / 2.0;    // 2/4인 경우
             double d = (weight * 3.0) / 4.0;    // 3/4인 경우
-
+            
             if (map.containsKey(a)) answer += map.get(a);
             if (map.containsKey(b)) answer += map.get(b);
             if (map.containsKey(c)) answer += map.get(c);
             if (map.containsKey(d)) answer += map.get(d);
-
+            
             map.put(a, map.getOrDefault(a, 0) + 1);
         }
         return answer;
