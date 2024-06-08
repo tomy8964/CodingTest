99414ae52e65e12f1af88f7eafaaceabb361f489
Geonwook Ham <tomy8964@naver.com>
2024-06-08 18:49:26 +0900

    [level 3] Title: 합승 택시 요금, Time: 2.39 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -2,6 +2,31 @@ import java.util.*;
 
 class Solution {
 
+    private static int[] getsDist(int n, int start, List<List<int[]>> edge) {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        boolean[] visited = new boolean[n + 1];
+        for (int i = 0; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for (int j = 1; j <= n; j++) {
+                if (!visited[j] && dist[j] < min) {
+                    min = dist[j];
+                    minIndex = j;
+                }
+            }
+            if (minIndex == -1) break;
+            visited[minIndex] = true;
+            for (int[] e : edge.get(minIndex)) {
+                if (!visited[e[0]] && dist[e[0]] > dist[minIndex] + e[1]) {
+                    dist[e[0]] = dist[minIndex] + e[1];
+                }
+            }
+        }
+        return dist;
+    }
+
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = 0;
         // S에서 모든 정점 가는 최단 거리 구하기
@@ -18,70 +43,9 @@ class Solution {
             edge.get(to).add(new int[]{from, cost});
         }
 
-        int[] sDist = new int[n + 1];
-        Arrays.fill(sDist, Integer.MAX_VALUE);
-        sDist[s] = 0;
-        boolean[] visited = new boolean[n + 1];
-        for (int i = 0; i < n; i++) {
-            int min = Integer.MAX_VALUE;
-            int minIndex = -1;
-            for (int j = 1; j <= n; j++) {
-                if (!visited[j] && sDist[j] < min) {
-                    min = sDist[j];
-                    minIndex = j;
-                }
-            }
-            if(minIndex == -1) break;
-            visited[minIndex] = true;
-            for (int[] e : edge.get(minIndex)) {
-                if (!visited[e[0]] && sDist[e[0]] > sDist[minIndex] + e[1]) {
-                    sDist[e[0]] = sDist[minIndex] + e[1];
-                }
-            }
-        }
-        // 도착한 정점으로 부터 B로 가는 거리 + A로 가는 거리
-        int[] bDist = new int[n + 1];
-        Arrays.fill(bDist, Integer.MAX_VALUE);
-        bDist[b] = 0;
-        visited = new boolean[n + 1];
-        for (int i = 0; i < n; i++) {
-            int min = Integer.MAX_VALUE;
-            int minIndex = -1;
-            for (int j = 1; j <= n; j++) {
-                if (!visited[j] && bDist[j] < min) {
-                    min = bDist[j];
-                    minIndex = j;
-                }
-            }
-            if(minIndex == -1) break;
-            visited[minIndex] = true;
-            for (int[] e : edge.get(minIndex)) {
-                if (!visited[e[0]] && bDist[e[0]] > bDist[minIndex] + e[1]) {
-                    bDist[e[0]] = bDist[minIndex] + e[1];
-                }
-            }
-        }
-        int[] aDist = new int[n + 1];
-        Arrays.fill(aDist, Integer.MAX_VALUE);
-        aDist[a] = 0;
-        visited = new boolean[n + 1];
-        for (int i = 0; i < n; i++) {
-            int min = Integer.MAX_VALUE;
-            int minIndex = -1;
-            for (int j = 1; j <= n; j++) {
-                if (!visited[j] && aDist[j] < min) {
-                    min = aDist[j];
-                    minIndex = j;
-                }
-            }
-            if(minIndex == -1) break;
-            visited[minIndex] = true;
-            for (int[] e : edge.get(minIndex)) {
-                if (!visited[e[0]] && aDist[e[0]] > aDist[minIndex] + e[1]) {
-                    aDist[e[0]] = aDist[minIndex] + e[1];
-                }
-            }
-        }
+        int[] sDist = getsDist(n, s, edge);
+        int[] bDist = getsDist(n, a, edge);
+        int[] aDist = getsDist(n, b, edge);
 
         int[] dist = new int[n + 1];
         for (int i = 0; i <= n; i++) {
